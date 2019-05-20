13862a9d69bc29f36aca8db5160c7f02fbe62f5f
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-20 14:59:10 -0700

    creates and runs egyptianwar

--- RunGame.java
+++ RunGame.java
@@ -1,1 +1,24 @@
+import javax.swing.JFrame;
+import java.awt.Component;
 
+public class TheGame extends JFrame
+{
+  private static final int WIDTH = 800;
+  private static final int HEIGHT = 600;
+  
+  public TheGame()
+  {
+    super("EGYPTIAN WAR");
+    setSize(WIDTH,HEIGHT);
+    EgyptianWar game = new EgyptianWar(WIDTH,HEIGHT);
+    ((Component)game).setFocusable(true);
+    getContentPane().add(game);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
+  public static void main(String args[])
+  {
+    TheGame run = new TheGame();
+  }
+}
