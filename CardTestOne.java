42a358a09ca1a7b56f29238bb6ff956a9e12108f
Chris Wooden <cmwooden@gmail.com>
2019-05-21 14:06:41 -0700

    Added testing

---
+++ CardTestOne.java
@@ -0,0 +1,31 @@
+import static java.lang.System.*;
+import java.awt.Color;
+
+public class CardTestOne
+{
+  public static void main( String args[] )
+  {
+    Card one = new Card();
+    out.println(one);
+
+    Card two = new Card(1,"DIAMONDS");
+    out.println(two);
+
+    Card three = new Card(4,"CLUBS");
+    out.println(three);
+                
+    Card four = new Card(12,"SPADES");
+    out.println(four);
+        
+    Card five = new Card(12,"HEARTS");
+    out.println(five);  
+                
+    Card six = new Card(9,"SPADES");
+    out.println(six);                           
+
+    out.println(one.equals(two));
+    out.println(one.equals(one));               
+    out.println(four.equals(five));     
+    out.println(three.equals(four));                                            
+  }
+}
