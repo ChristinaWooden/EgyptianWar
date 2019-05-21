142ae9f3ac6e3bdd65f3978659626bed2f939a5f
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-21 14:46:41 -0700

    egyptian war progress

--- EgyptianWar.java
+++ EgyptianWar.java
@@ -10,28 +10,49 @@ import java.awt.image.BufferedImage;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class EgyptianWar{
+public class EgyptianWar
+{
 	private Deck deck;
 	private boolean[] keys;
 	private BufferedImage back;
-
-	public EgyptianWar(){
+	
+	public EgyptianWar()
+	{
 		deck = new Deck();
 		keys = new boolean[3];
+		setBackground(Color.WHITE);
+		setVisible(true);  
+		new Thread(this).start();
+		addKeyListener(this);
 	}
-
-	public void update(Graphics window){
+	
+	public void update(Graphics window)
+	{
 		paint(window);
 	}
-
-	public void paint(Graphics window){
-		
+	
+	public void paint(Graphics window)
+	{
+	
 	}
 
-
-
-
-
-
+	public void keyPressed(KeyEvent e)
+	{
+		switch(toUpperCase(e.getKeyChar()))
+		{
+			case 'SPACE':keys[0]=true;break;
+			case '1':keys[1]=true;break;
+			case 'N':keys[2]=true;break;
+		}
+	}
 	
+	public void keyReleased(KeyEvent e)
+	{
+		switch(toUpperCase(e.getKeyChar()))
+		{
+			case 'SPACE':keys[0]=false;break;
+			case '1':keys[1]=false;break;
+			case 'N':keys[2]=false;break;
+		}
+	}
 }
