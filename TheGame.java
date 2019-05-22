d73590b657bec02053953ffe89b2a49934ce9532
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-22 15:00:46 -0700

    added player numbers

--- TheGame.java
+++ TheGame.java
@@ -1,16 +1,21 @@
 import javax.swing.JFrame;
 import java.awt.Component;
+import java.util.Scanner;
 
 public class TheGame extends JFrame
 {
   private static final int WIDTH = 800;
   private static final int HEIGHT = 600;
+  private int players;
   
   public TheGame()
   {
+    Scanner k=new Scanner(System.in);
+    System.out.print("Enter number of players: ");
+    players=k.nextInt();
     super("EGYPTIAN WAR");
     setSize(WIDTH,HEIGHT);
-    EgyptianWar game = new EgyptianWar(WIDTH,HEIGHT);
+    EgyptianWar game = new EgyptianWar(WIDTH,HEIGHT,players);
     ((Component)game).setFocusable(true);
     getContentPane().add(game);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
