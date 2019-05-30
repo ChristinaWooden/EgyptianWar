d128e3a4c2a3abb343c02aba7ea6c3bfc6e9fbf5
BachVinylDiscs <buskirk.max@gmail.com>
2019-05-30 15:00:02 -0700

    mahogany

--- Mahogany.java
+++ Mahogany.java
@@ -1,1 +1,37 @@
+import java.io.File;
+import java.net.URL;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.imageio.ImageIO;
 
+public class Mahogany
+{
+  private Image image;
+  private int width;
+  private int height;
+
+  public Mahogany(int w, int h)
+  {
+    width=w;
+    height=h;
+    try
+    {
+      URL url = getClass().getResource("images/mahogany.jpg");
+      image = ImageIO.read(url);
+    }
+    catch(Exception e)
+    {
+      //mahogany
+    }
+  }
+
+  public void draw(Graphics window)
+  {
+    window.drawImage(image,0,0,width,height,null);
+  }
+
+  public String toString()
+  {
+    return "A picture is worth a thousand words";
+  }
+}
