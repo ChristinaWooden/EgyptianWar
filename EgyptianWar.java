778646c257db7e73a4158cb8d1ceef20e320cff6
BachVinylDiscs <buskirk.max@gmail.com>
2019-06-04 14:15:10 -0700

    commit first

--- EgyptianWar.java
+++ EgyptianWar.java
@@ -212,14 +212,8 @@ public class EgyptianWar extends Canvas implements KeyListener, Runnable
 		twoDGraph.drawImage(image, 0, 0, null);*/
 		mahogany.draw(graphToBack);
 		center.add(deck.nextCard());
-		if (center.size() > 4){
-			upperDisplay=Math.min(4,center.size()-1);
-		}
-		else {
-			upperDisplay = center.size();
-		}
-		for(int i=upperDisplay;i>0;i--){
-		  (center.get(i)).draw(graphToBack,(10+(upperDisplay-i)*91),10,365,485);
+		upperDisplay=Math.min(4,center.size()-1);
+		for(int i=upperDisplay;i>=0;i--){
 		  (center.get(i)).draw(graphToBack,(10+(upperDisplay-i)*91),10,365,485);
 		}
 		center.add(deck.nextCard());
