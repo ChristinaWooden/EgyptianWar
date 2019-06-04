e440768813dec7d42c9a1cbadd2af7f4cc5a8a6b
Chris Wooden <cmwooden@gmail.com>
2019-06-04 14:17:26 -0700

    sad push

--- EgyptianWar.java
+++ EgyptianWar.java
@@ -211,7 +211,6 @@ public class EgyptianWar extends Canvas implements KeyListener, Runnable
     		}
 		twoDGraph.drawImage(image, 0, 0, null);*/
 		mahogany.draw(graphToBack);
-<<<<<<< HEAD
 		if (center.size() > 4){
 			upperDisplay=Math.min(4,center.size()-1);
 		}
@@ -220,11 +219,10 @@ public class EgyptianWar extends Canvas implements KeyListener, Runnable
 		}
 		for(int i=upperDisplay;i>0;i--){
 		  (center.get(i)).draw(graphToBack,(10+(upperDisplay-i)*91),10,365,485);
-=======
+		}
 		center.add(deck.nextCard());
 		upperDisplay=Math.min(4,center.size()-1);
 		for(int i=upperDisplay;i>=0;i--){
->>>>>>> 778646c257db7e73a4158cb8d1ceef20e320cff6
 		  (center.get(i)).draw(graphToBack,(10+(upperDisplay-i)*91),10,365,485);
 		}
 		graphToBack.setColor(Color.WHITE);
