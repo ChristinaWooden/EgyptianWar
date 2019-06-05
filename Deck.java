52fc7ed02512a17c6d924b288b17db8175e8c794
bindleya8396 <42781299+bindleya8396@users.noreply.github.com>
2019-06-05 14:16:35 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -1,8 +1,7 @@
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Deck
-{
+public class Deck {
   public static final int NUMFACES = 13;
   public static final int NUMSUITS = 4;
   public static final int NUMCARDS = 52;
@@ -12,57 +11,47 @@ public class Deck
   private int topCardIndex;
   private ArrayList<Card> stackOfCards;
 
-  public Deck ()
-  {
+  public Deck () {
     //initialize data - stackOfCards - topCardIndex
     stackOfCards = new ArrayList<Card>();         
     Card c1 = new Card();   
     topCardIndex = NUMCARDS -1 ;
                 
-    for (int s = 0; s < 4; s++){
-    	for (int f = 0; f < 13; f++){
+    for (int s = 0; s < 4; s++) {
+    	for (int f = 0; f < 13; f++) {
     		c1 = new Card(f,SUITS[s]);
     		stackOfCards.add(c1);
     	}
     }
-    //loop through suits
-    //loop through faces
-    //add in a new card
-                
   }
 
   //modifiers
-  public void shuffle ()
-  {
+  public void shuffle () {
     Collections.shuffle(stackOfCards);
     topCardIndex = stackOfCards.size() -1;
   }
 
   //accessors
-  public int size ()
-  {
+  public int size () {
     return stackOfCards.size();
   }
 
-  public int numCardsLeft()
-  {
+  public int numCardsLeft() {
     return topCardIndex + 1;
   }
 
-  public Card nextCard()
-  {
+  public Card nextCard() {
   	if (topCardIndex < 0){
   		return null;
   	}
     return stackOfCards.get(topCardIndex--);
   }
 
-  public Card remove(int i){
+  public Card remove(int i) {
     return stackOfCards.remove(i);
   }
 
-  public String toString()
-  {
+  public String toString() {
     return stackOfCards + "   topCardIndex = " + topCardIndex;
   } 
 }
